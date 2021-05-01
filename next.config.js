c9e1cd382f47a371a8183c99be5df98a7dface02
leonunesbs <leonunesbs@gmail.com>
2021-05-01 20:58:57 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -5,13 +5,6 @@ module.exports = withPWA({
     dest: "public",
     disable: process.env.NODE_ENV === "development",
     register: true,
-    fallbacks: {
-      image: "/static/images/fallback.jpg",
-      // document: '/other-offline',  // if you want to fallback to a custom    page other than /_offline
-      // font: '/static/font/fallback.woff2',
-      // audio: ...,
-      // video: ...,
-    },
     cacheOnFrontEndNav: true,
   },
 });
