d19800da3c59b2506eb3e6ad1fd5ade944d59a35
leonunesbs <leonunesbs@gmail.com>
2021-05-01 00:12:41 -0300

    padding

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -3,7 +3,7 @@ import { Flex, Link } from "@chakra-ui/react";
 
 const Nav = ({ categories }) => {
   return (
-    <Flex p={6} boxShadow="sm">
+    <Flex px={6} py={4} boxShadow="sm">
       <Flex as="nav" flexGrow={1} justify="space-between">
         <Flex>
           <Link href="/" fontWeight="bold" _hover={{ textDecoration: "none" }}>
