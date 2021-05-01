15acc5cb6bbd66a016c736b7b3eec20e6d3aea20
leonunesbs <leonunesbs@gmail.com>
2021-05-01 18:32:02 -0300

    remove unused scripts

--- src/pages/_app/index.tsx
+++ src/pages/_app/index.tsx
@@ -20,13 +20,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Staatliches"
         />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css"
-        />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js" />
-        <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js" />
       </Head>
       <ChakraProvider theme={theme}>
         <GlobalContext.Provider value={global}>
