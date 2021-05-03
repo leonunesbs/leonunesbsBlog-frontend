11de58a0b7c37952a4b5631e47360aca2432d451
leonunesbs <leonunesbs@gmail.com>
2021-05-03 00:00:27 -0300

    sanitizin

--- src/components/tissues/nav/Nav.d.ts
+++ src/components/tissues/nav/Nav.d.ts
@@ -1,7 +1,23 @@
-import { FlexProps } from "@chakra-ui/react";
+import { CollapseProps, FlexProps, UseDisclosureProps } from "@chakra-ui/react";
 import { CategoryProps, HomepageProps } from "../../../types/Types";
 
 export interface NavProps extends FlexProps {
+  categories: CategoryProps[];
+  homepage: HomepageProps;
+}
+
+interface UseDisclosureExtendedProps extends UseDisclosureProps {
+  onToggle: () => void;
+}
+
+export interface LeftNavProps {}
+export interface RightNavProps {
+  categories: CategoryProps[];
+  mobileNavDisclosure: UseDisclosureExtendedProps;
+}
+
+export interface MobileNavProps extends CollapseProps {
+  mobileNavDisclosure: UseDisclosureExtendedProps;
   categories: CategoryProps[];
   homepage: HomepageProps;
 }
