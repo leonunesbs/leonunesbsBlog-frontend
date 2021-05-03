6b09ced3e068853a02e2e040c8479e71ce8d4b26
leonunesbs <leonunesbs@gmail.com>
2021-05-02 22:41:55 -0300

    add footer

--- src/components/tissues/footer/index.tsx
+++ src/components/tissues/footer/index.tsx
@@ -1,5 +1,47 @@
-function Footer({ categories }) {
-  return <></>;
+import React from "react";
+import {
+  Flex,
+  useColorModeValue,
+  Image as ChakraImage,
+} from "@chakra-ui/react";
+
+import NextLink from "next/link";
+import { Divider, Social } from "../..";
+
+function Footer({ categories, homepage }) {
+  const bg = useColorModeValue("gray.200", "gray.800");
+
+  const logoSrc = useColorModeValue(
+    "/static/logo-light-notext.svg",
+    "/static/logo-dark-notext.svg"
+  );
+  const altSrc = useColorModeValue("logo-light-notext", "logo-dark-notext");
+  return (
+    <Flex
+      px={6}
+      py={4}
+      boxShadow="md"
+      minH="120px"
+      bgColor={bg}
+      flexDir="column"
+      align="center"
+      justify="center"
+    >
+      <NextLink as="/" href="/">
+        <a>
+          <ChakraImage
+            objectFit="contain"
+            boxSize={["80px", "100px"]}
+            minW="80px"
+            src={logoSrc}
+            alt={altSrc}
+          />
+        </a>
+      </NextLink>
+      <Divider my={4} maxW="50%" />
+      <Social homepage={homepage} />
+    </Flex>
+  );
 }
 
 export default Footer;
