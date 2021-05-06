91dc83591885fa224aae2118bf26165cf7e38ffe
leonunesbs <leonunesbs@gmail.com>
2021-05-06 11:24:13 -0300

    update hamburger

--- src/components/tissues/nav/RightNav.tsx
+++ src/components/tissues/nav/RightNav.tsx
@@ -1,4 +1,11 @@
-import { Flex, Icon, Link, Stack, useColorModeValue } from "@chakra-ui/react";
+import {
+  Flex,
+  Icon,
+  Link,
+  Stack,
+  useColorModeValue,
+  useTheme,
+} from "@chakra-ui/react";
 import { IoMdMenu } from "react-icons/io";
 import NextLink from "next/link";
 import { RightNavProps } from "./Nav";
@@ -7,8 +14,20 @@ import { ColorModeSwitch } from "../../cells";
 import { Squash as Hamburger } from "hamburger-react";
 
 function RightNav({ categories, mobileNavDisclosure }: RightNavProps) {
+  const theme = useTheme();
+
   const color = useColorModeValue("gray.700", "gray.50");
   const brand = useColorModeValue("brand.500", "brand.300");
+  const hamburgerActiveColor = useColorModeValue(
+    theme.colors.brand[500],
+    theme.colors.brand[300]
+  );
+
+  const hamburgerDefaultColor = useColorModeValue(
+    theme.colors.gray[700],
+    theme.colors.gray[50]
+  );
+
   return (
     <>
       <Stack isInline spacing={6} align="center">
@@ -31,15 +50,15 @@ function RightNav({ categories, mobileNavDisclosure }: RightNavProps) {
             </Flex>
           );
         })}
-        <Flex
-          display={["flex", "flex", "flex", "none"]}
-          cursor="pointer"
-          _hover={{ color: brand }}
-          color={mobileNavDisclosure.isOpen && brand}
-        >
+        <Flex display={["flex", "flex", "flex", "none"]} cursor="pointer">
           <Hamburger
             size={25}
             distance="sm"
+            color={
+              mobileNavDisclosure.isOpen
+                ? hamburgerActiveColor
+                : hamburgerDefaultColor
+            }
             toggle={mobileNavDisclosure.onOpen}
             toggled={mobileNavDisclosure.isOpen}
             onToggle={(toggle) =>
